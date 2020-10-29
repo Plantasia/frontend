7bdb11d45b56543ea28a710047bc73ca99fa6898
Matheus Faggi <matheusfaggi@gmail.com>
2020-10-29 16:44:10 -0300

    add notfound page

---
+++ src/pages/404.tsx
@@ -0,0 +1,10 @@
+import { NOTFOUND } from "dns"
+
+export default function PageNotFound() {
+  return (
+    <>
+      <h1>Are you lost my bro? </h1>
+      <a href="/">Back to home page</a>
+    </>
+  )
+}
