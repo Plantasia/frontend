570598a54b5ac7d1c0bee8f7a35df8bb2abd0073
Matheus Faggi <matheusfaggi@gmail.com>
2020-11-13 01:32:59 -0300

    boilerplate: set bootstrap as dom root

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,15 +2,18 @@
 import GlobalStyle from "../styles/GlobalStyle"
 import { Grommet } from "grommet"
 import { deepTheme } from "@/components/GrommetTheme"
+import { Container } from "reactstrap"
 import "bootstrap/dist/css/bootstrap.min.css"
 
 function MyApp({ Component, pageProps }) {
   return (
     <>
-      <Grommet className="App" theme={deepTheme}>
-        <GlobalStyle />
-        <Component {...pageProps} />
-      </Grommet>
+      <Container className="pl-1 pr-1">
+        <Grommet className="App" theme={deepTheme}>
+          <GlobalStyle />
+          <Component {...pageProps} />
+        </Grommet>
+      </Container>
     </>
   )
 }
