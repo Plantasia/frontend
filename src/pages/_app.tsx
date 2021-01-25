3e42b0df2d63ba12ce48169dce214db49d9a957c
Matheus Faggi <matheusfaggi@gmail.com>
2021-01-24 21:15:00 -0300

    remove useless import

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,7 +2,6 @@
 import GlobalStyle from "../styles/GlobalStyle"
 import "bootstrap/dist/css/bootstrap.min.css"
 import Head from "next/head"
-import Link from "next/link"
 
 function MyApp({ Component, pageProps }) {
   return (
