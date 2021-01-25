a5c856e5cbad20862092870e3933ec31cbf8cbd9
Matheus Faggi <matheusfaggi@gmail.com>
2021-01-24 21:16:21 -0300

    Add container wrapping all pages

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -7,6 +7,8 @@ import Document, {
 } from "next/document"
 import { ServerStyleSheet } from "styled-components"
 
+import { Container } from "react-bootstrap"
+
 export default class MyDocument extends Document {
   static async getInitialProps(ctx: DocumentContext) {
     const sheet = new ServerStyleSheet()
@@ -39,14 +41,17 @@ export default class MyDocument extends Document {
         <Head>
           <meta charSet="utf-8" />
 
-          <link rel="preconnect" href="https://fonts.gstatic.com"/>
-          <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,700;1,400&display=swap" rel="stylesheet"/>
-
-
+          <link rel="preconnect" href="https://fonts.gstatic.com" />
+          <link
+            href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,700;1,400&display=swap"
+            rel="stylesheet"
+          />
         </Head>
         <body>
-          <Main />
-          <NextScript />
+          <Container>
+            <Main />
+            <NextScript />
+          </Container>
         </body>
       </Html>
     )
