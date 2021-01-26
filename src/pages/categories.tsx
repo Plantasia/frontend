78281e5dec0a029b616724b5642bf81781319fc6
icarodemorais <icarodemorais@hotmail.com>
2021-01-26 20:07:02 -0300

    fix header component

--- src/pages/categories.tsx
+++ src/pages/categories.tsx
@@ -1,12 +1,16 @@
 import { Header } from "@components"
 import CategoryListingOneItem from "../components/CategoryListingItem"
 import CategoryBeforeItems from "@components/CategoryBeforeItems"
+import { Button } from "react-bootstrap"
 
 export default function ListCategories() {
   return (
     <>
-      <Header actionText="Registre-se" />
-
+      <Header>
+        {{
+          right: <Button>Registre-se aqui </Button>,
+        }}
+      </Header>
       <CategoryBeforeItems />
 
       <CategoryListingOneItem />
