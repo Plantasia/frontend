2f6546fea2edc2bcf59cc427ed6d272a2a2430a2
Matheus Faggi <matheusfaggi@gmail.com>
2021-03-14 04:36:57 -0300

    chore: create protocol file

--- src/services/Categories.ts
+++ src/services/Categories.ts
@@ -1,41 +1,15 @@
 /* eslint-disable camelcase */
 import axios from "axios"
 import { CategoryProps } from "@src/pages/category/_categoryItem"
-
-export interface CategoryAPI {
-  name: string
-  id: string
-  description: string
-  authorId: string
-  lastComment: {
-    created_at: string
-  }
-  qtdeComments?: number
-  qtdeTopics: number
-  lastTopic: {
-    id: string
-    name: string
-    textBody: string
-    imageStore: string
-    isActive: boolean
-    created_at: string
-  }
-}
-
-export type TopicAPI = {
-  id: string
-  name: string
-  textBody: string
-  imageStorage: string
-  created_at: string
-  updated_at: string
-}
+import { CategoryAPI } from "./protocols"
 
 export const GetCategories = async (): Promise<CategoryProps[]> => {
   const { data } = await axios.get<{ categories: CategoryAPI[] }>(
     "http://backend:3333/forum/categories/"
   )
-  const response = data.categories.map(
+  // @TO-DO tratar exceções
+  return data.categories.map(
+    // API keys
     ({
       authorId,
       description,
@@ -45,17 +19,17 @@ export const GetCategories = async (): Promise<CategoryProps[]> => {
       name,
       qtdeTopics,
       qtdeComments,
-    }) => ({
-      id,
-      repliesCount: qtdeComments || 0,
-      topicsCount: qtdeTopics || 0,
-      name,
-      description,
-      lastActivity: lastComment === undefined ? "" : lastComment.created_at,
-      // FAILURE API COMPATIBILITY
-      lastTopic: null,
-    })
+    }) =>
+      // Component props
+      ({
+        id,
+        repliesCount: qtdeComments || 0,
+        topicsCount: qtdeTopics || 0,
+        name,
+        description,
+        lastActivity: lastComment === undefined ? "" : lastComment.created_at,
+        // FAILURE API COMPATIBILITY
+        lastTopic: null,
+      })
   )
-
-  return response
 }
---
+++ src/services/protocols.d.ts
@@ -0,0 +1,30 @@
+/* eslint-disable camelcase */
+
+export interface CategoryAPI {
+  name: string
+  id: string
+  description: string
+  authorId: string
+  lastComment: {
+    created_at: string
+  }
+  qtdeComments?: number
+  qtdeTopics: number
+  lastTopic: {
+    id: string
+    name: string
+    textBody: string
+    imageStore: string
+    isActive: boolean
+    created_at: string
+  }
+}
+
+export type TopicAPI = {
+  id: string
+  name: string
+  textBody: string
+  imageStorage: string
+  created_at: string
+  updated_at: string
+}
