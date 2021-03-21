b70e5b68f0be52091dc619b6e9aa1525f81d0d5c
Matheus Faggi <matheusfaggi@gmail.com>
2021-03-21 19:21:49 -0300

    context changes

--- src/contexts/User.tsx
+++ src/contexts/User.tsx
@@ -1,32 +1,71 @@
+/* eslint-disable camelcase */
+/* eslint-disable no-undef */
+
 import { UserProps } from "@utils/types"
-import React, { createContext, Dispatch, useEffect, useReducer } from "react"
-import { UserReducer, UserState, UserAction } from "./reducer/User"
+import React, {
+  createContext,
+  Dispatch,
+  useEffect,
+  useReducer,
+  useState,
+} from "react"
+import { ClientSideApi } from "@src/services/Api"
+
+// WITH REDUCER
+// export type UserContextTypeWithReducer = {
+//   state: UserState
+//   dispatch: Dispatch<UserAction>
+// }
+// const initialState: UserState = {
+//   isLoading: false,
+//   user: null,
+//   error: null,
+// }
+
+//
 
 export type UserContextType = {
-  state: UserState
-  dispatch: Dispatch<UserAction>
+  userToken: string
+  signIn(email: string, password: string): Promise<void>
+  signOut(): void
 }
 
-const initialState: UserState = {
-  isLoading: false,
-  user: null,
-  error: null,
-}
-
-export const UserContext = createContext<UserContextType>({
-  dispatch: () => null,
-  state: initialState,
-})
+export const UserContext = createContext<UserContextType>(null)
 
 export const UserContextProvider: React.FC = ({ children }) => {
-  const [state, dispatch] = useReducer(UserReducer, initialState)
+  const [userToken, setUserToken] = useState<string>(null)
+
   useEffect(() => {
-    // eslint-disable-next-line no-undef
-    if (localStorage.getItem("@PLTuser")) dispatch({ type: "success" })
+    if (localStorage.getItem("@PLTuser")) {
+      const token = localStorage.getItem("@PLTuser")
+      setUserToken(token)
+    }
   }, [])
 
+  const signIn = async (email, password) => {
+    const response = await ClientSideApi.post<{ access_token: string }>(
+      "/signin",
+      { data: { email, password } }
+    )
+    console.log(response)
+    if (response.data) {
+      const { access_token } = response.data
+      setUserToken(access_token)
+      localStorage.setItem("@PLTuser", access_token)
+    }
+  }
+  const signOut = () => {
+    setUserToken(null)
+  }
+
   return (
-    <UserContext.Provider value={{ state, dispatch }}>
+    <UserContext.Provider
+      value={{
+        userToken,
+        signIn,
+        signOut,
+      }}
+    >
       {children}
     </UserContext.Provider>
   )
