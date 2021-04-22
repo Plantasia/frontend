8071c79dc9a34b2162bb8554776b3ac5eaa1becb
Matheus Faggi <matheusfaggi@gmail.com>
2021-04-21 22:08:14 -0300

    ✨ create component flash alert with event bus

---
+++ src/components/FlashAlert.tsx
@@ -0,0 +1,36 @@
+import React, { ReactElement, useEffect, useState } from "react"
+import { Alert, AlertProps } from "react-bootstrap"
+import styled from "styled-components"
+import Bus from "@utils/Bus"
+
+export const ErrorAlert = styled(Alert).attrs(
+  (): AlertProps => ({
+    variant: "danger",
+    className: "mb-3",
+  })
+)``
+const FixedAlert = styled(Alert)`
+  position: fixed;
+  right: 15px;
+  top: 15px;
+  left: auto;
+`
+
+export const FlashAlert: React.FC = props => {
+  const [visibility, setVisibility] = useState(false)
+  const [message, setMessage] = useState("")
+  const [type, setType] = useState("")
+
+  useEffect(() => {
+    Bus.addListener("flash", ({ message, type }) => {
+      setVisibility(true)
+      setMessage(message)
+      setType(type)
+      setTimeout(() => {
+        setVisibility(false)
+      }, 4000)
+    })
+  }, [])
+
+  return visibility && <FixedAlert variant={type}>{message}</FixedAlert>
+}
