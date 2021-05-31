badc7aa215454baf9fb9295aa8888cbc60f41a90
Matheus Faggi <matheusfaggi@gmail.com>
2021-05-31 18:04:26 -0300

    refactor newComment

--- src/pages/topics/[id]/index.tsx
+++ src/pages/topics/[id]/index.tsx
@@ -1,14 +1,13 @@
-import { Button, Row, Col } from "react-bootstrap"
+import { Button, Row, Col, Image } from "react-bootstrap"
 import { Editor, AppLayout, RequestAuthModal, SEO } from "@components"
 import { InlineGap, PlantasiaCard } from "@styled/Shared"
 import { CommentProps, Comment } from "./_comment"
 import { ComponentProps } from "@utils/types"
-import { useEffect, useState } from "react"
-import { GetTopic } from "@src/services/Topics"
+import { useState } from "react"
 import { SelfApi } from "@src/services/Api"
 import { useUser } from "@src/lib"
 import { useRouter } from "next/router"
-import useSWR, { mutate } from "swr"
+import useSWR from "swr"
 import { axiosFetcher } from "@src/lib/fetchJson"
 import { FaComment } from "react-icons/fa"
 export interface BadgeCategoryProps {
@@ -22,6 +21,7 @@ export interface TopicProps {
   categories: BadgeCategoryProps[]
   comments: CommentProps[]
   author: ComponentProps.UserProps
+  image: string
 }
 
 export default function ShowTopic(props) {
@@ -36,43 +36,42 @@ export default function ShowTopic(props) {
   )
 
   const submitNewComment = async () => {
-    if (user?.isLoggedIn) {
-      const previewComment: CommentProps = {
-        id: "teste",
-        createdAt: "agora mesmo",
-        content: newComment,
-        ownerUser: {
-          id: user.id,
-          avatar: user.avatar,
-          bio: user.bio,
-          name: user.name,
-          createdAt: user.createdAt,
-        },
-      }
-      mutateTopic(data => {
-        const comments = data.comments
-          ? [...data.comments, previewComment]
-          : [previewComment]
+    if (!user?.isLoggedIn) return setModalVisible(true)
 
-        return {
-          ...data,
-          comments,
-        }
-      }, false)
-
-      const commentPayload = {
-        textBody: newComment,
-        topic_id: router.query.id,
-      }
-
-      const {
-        data: { message, type },
-      } = await SelfApi.post("/api/comment", commentPayload)
-      window.flash(message, type)
-      setNewComment("")
-    } else {
-      setModalVisible(true)
+    const previewComment: CommentProps = {
+      id: "teste",
+      createdAt: "agora mesmo",
+      content: newComment,
+      ownerUser: {
+        id: user.id,
+        avatar: user.avatar,
+        bio: user.bio,
+        name: user.name,
+        createdAt: user.createdAt,
+      },
     }
+
+    mutateTopic(data => {
+      const comments = data.comments
+        ? [...data.comments, previewComment]
+        : [previewComment]
+
+      return {
+        ...data,
+        comments,
+      }
+    }, false)
+
+    const commentPayload = {
+      textBody: newComment,
+      topic_id: router.query.id,
+    }
+
+    const {
+      data: { message, type },
+    } = await SelfApi.post("/api/comment", commentPayload)
+    window.flash(message, type)
+    setNewComment("")
   }
   return (
     <AppLayout>
@@ -86,28 +85,37 @@ export default function ShowTopic(props) {
           <Row>
             <Col xs="12" className="mb-4">
               <h2 className="mb-3">{topic.title}</h2>
-              <p>{topic.description}</p>
-            </Col>
-            <Col xs="12" className="d-flex justify-content-between mb-3">
               <InlineGap>
                 {topic
-                  ? topic.categories.map(({ name, color }, i) => (
-                      <Button variant={`outline-${color}`} key={i} size="sm">
+                  ? topic.categories.map(({ name, color, id }, i) => (
+                      <Button
+                        variant={`outline-${color}`}
+                        key={i}
+                        size="sm"
+                        onClick={() => {
+                          router.push(`/topics?category=${id}`)
+                        }}
+                        className="mb-2"
+                      >
                         {name}
                       </Button>
                     ))
                   : null}
               </InlineGap>
-              <InlineGap>
-                <Button
-                  variant="primary"
-                  href="#new-comment"
-                  className="d-flex align-items-center"
-                >
-                  novo comentário
-                  <FaComment className="ml-2" />
-                </Button>
-              </InlineGap>
+              <p>{topic.description}</p>
+              {topic.image && (
+                <Image src={topic.image} className="mb-3 " fluid />
+              )}
+            </Col>
+            <Col xs="12" className="d-flex justify-content-end mb-3">
+              <Button
+                variant="primary"
+                href="#new-comment"
+                className="d-flex align-items-center"
+              >
+                novo comentário
+                <FaComment className="ml-2" />
+              </Button>
             </Col>
           </Row>
           <Row>
