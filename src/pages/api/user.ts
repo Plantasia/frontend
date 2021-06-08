17c2e13049f69d44f013e8bbb11df3bfdf704c7c
Matheus Faggi <matheusfaggi@gmail.com>
2021-06-07 23:49:16 -0300

    refactor: implement NextConnect

--- src/pages/api/user.ts
+++ src/pages/api/user.ts
@@ -2,30 +2,19 @@ import { ServerSideApi } from "@src/services/Api"
 import { Handler, withIronSession, Session } from "next-iron-session"
 import { NextApiRequest, NextApiResponse } from "next"
 import { sessionOptions } from "./_iron-session/helpers"
+import NextConnect from "next-connect"
 
-const handler: Handler = async (
-  req: NextApiRequest & { session: Session },
-  res: NextApiResponse
-) => {
-  const jwt = await req.session.get("jwt")
-  const headers = jwt
-    ? {
-        Authorization: `Bearer ${jwt}`,
-      }
-    : null
+const nc = NextConnect<
+  NextApiRequest & {
+    session: Session
+    file: any
+  },
+  NextApiResponse
+>()
+  .get(async (req, res) => {
+    const jwt = await req.session.get("jwt")
+    const headers = jwt && { Authorization: `Bearer ${jwt}` }
 
-  if (req.method === "PATCH") {
-    if (!headers)
-      res.status(401).json({ message: "Não autorizado", type: "danger" })
-
-    try {
-      const { data } = await ServerSideApi.patch("/user", req.body)
-      console.log(data)
-    } catch ({ response }) {
-      console.log(response)
-    }
-  }
-  if (req.method === "GET") {
     if (!headers) return res.json({ isLoggedIn: false })
     try {
       const { data: user } = await ServerSideApi.get(`/users/findme`, {
@@ -40,7 +29,22 @@ const handler: Handler = async (
       req.session.save()
       res.json({ error: "Sua sessão caiu, por favor logue novamente" })
     }
-  }
-}
+  })
+  .post(async (req, res) => {
+    const jwt = await req.session.get("jwt")
+    const headers = jwt && { Authorization: `Bearer ${jwt}` }
 
-export default withIronSession(handler, sessionOptions)
+    if (req.method === "PATCH") {
+      if (!headers)
+        res.status(401).json({ message: "Não autorizado", type: "danger" })
+
+      try {
+        const { data } = await ServerSideApi.patch("/user", req.body)
+        console.log(data)
+      } catch ({ response }) {
+        console.log(response)
+      }
+    }
+  })
+
+export default withIronSession(nc, sessionOptions)
