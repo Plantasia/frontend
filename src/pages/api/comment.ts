2664018f891dc5660ff764d7a4cd850b604727ec
Matheus Faggi <matheusfaggi@gmail.com>
2021-06-12 20:26:02 -0300

    refactor:  using next-connect

--- src/pages/api/comment.ts
+++ src/pages/api/comment.ts
@@ -3,32 +3,38 @@ import { Handler, withIronSession, Session } from "next-iron-session"
 import { ServerSideApi } from "@src/services/Api"
 import { sessionOptions } from "./_iron-session/helpers"
 import { NextApiRequest, NextApiResponse } from "next"
+import NextConnect from "next-connect"
 
-const handler: Handler = async (
-  req: NextApiRequest & { session: Session },
-  res: NextApiResponse
-) => {
-  const { body } = req
-  const { textBody, topic_id } = body
-  try {
-    const { status } = await ServerSideApi.post(
-      "/forum/comments",
-      {
-        textBody,
-        topic_id,
-      },
-      {
-        headers: { Authorization: `Bearer ${req.session.get("jwt")}` },
-      }
-    )
-    res.status(status).json({ message: "comentário criado", type: "success" })
-  } catch ({ response }) {
-    const {
-      data: { message },
-    } = response
+const nc = NextConnect<
+  NextApiRequest & {
+    session: Session
+    file: any
+  },
+  NextApiResponse
+>()
+  .post(async (req, res) => {
+    const { body } = req
+    const { textBody, topic_id } = body
+    try {
+      const { status } = await ServerSideApi.post(
+        "/forum/comments",
+        {
+          textBody,
+          topic_id,
+        },
+        {
+          headers: { Authorization: `Bearer ${req.session.get("jwt")}` },
+        }
+      )
+      res.status(status).json({ message: "comentário criado", type: "success" })
+    } catch ({ response }) {
+      const {
+        data: { message },
+      } = response
 
-    res.json({ message, type: "danger" })
-  }
-}
+      res.json({ message, type: "danger" })
+    }
+  })
+  .patch(async (req, res) => {})
 
-export default withIronSession(handler, sessionOptions)
+export default withIronSession(nc, sessionOptions)
