91b6357cebff0d3b98f77fa68701b9931ecbc605
Matheus Faggi <matheusfaggi@gmail.com>
2021-06-20 23:48:16 -0300

    refacor: using ApiHandler

--- src/pages/api/topic/[id].ts
+++ src/pages/api/topic/[id].ts
@@ -4,12 +4,22 @@ import { sessionOptions } from "../../../lib/iron-session/helpers"
 import { NextApiRequest, NextApiResponse } from "next"
 import { GetTopic } from "@src/services/Topics"
 import { ServerSideApi } from "@src/services/Api"
+import { ApiHandler } from "@src/lib/helpers"
 
-const handler: Handler = async (
-  req: NextApiRequest & { session: Session },
-  res: NextApiResponse
-) => {
-  if (req.method === "DELETE") {
+const handler: Handler = ApiHandler()
+  .get(async (req, res) => {
+    const {
+      query: { id },
+    } = req
+
+    try {
+      return res.json(await GetTopic(id as string))
+    } catch (error) {
+      console.log(error)
+      return res.status(500).json({ message: "deu ruim", type: "danger" })
+    }
+  })
+  .delete(async (req, res) => {
     const { id } = req.query
 
     try {
@@ -29,17 +39,6 @@ const handler: Handler = async (
       } = response
       return res.json({ message, type: "danger" })
     }
-  }
-  const {
-    query: { id },
-  } = req
-
-  try {
-    return res.json(await GetTopic(id as string))
-  } catch (error) {
-    console.log(error)
-    return res.status(500).json({ message: "deu ruim", type: "danger" })
-  }
-}
+  })
 
 export default withIronSession(handler, sessionOptions)
